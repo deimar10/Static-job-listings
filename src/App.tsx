56f8ab23bf50a61cb4c0f8cd92e63b8f3fc29edc
Deimar <deimarparn14@gmail.com>
2023-02-01 15:44:19 +0200

    Implemented the use of data json and interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { JobInterface } from './Interface/interface';
 import JobOffers from './components/jobOffers';
+import Data from './data.json';
 
 function App() {
+
+  const [jobData, setJobData] = useState<JobInterface[]>();
+
+  useEffect(() => {
+    setJobData(Data as JobInterface[])
+  }, []);
+
+  console.log(jobData)
+
   return (
     <div className="App">
       <div className='image-container'>
