482fa83e454feeb919639d03e0c39faba5276cd6
Deimar <deimarparn14@gmail.com>
2023-02-01 16:05:57 +0200

    Created Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Home.css';
+import JobOffers from '../components/jobOffers';
+import Illustration from '../images/bg-header-desktop.svg';
+
+function Home ({jobData}: any) {
+    return (
+        <div>
+            <div className='image-container' style={{
+                backgroundImage: `url(${Illustration})`
+            }}>
+            </div>
+            <div className='job-offers-container'>
+                <JobOffers jobData={jobData} />
+            </div>
+        </div>
+    )
+}
+
+export default Home;
